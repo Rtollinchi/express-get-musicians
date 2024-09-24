6ee2feec56559515540feef434bc2813ce10927c
Rtollinchi <rtollinchi23@gmail.com>
2024-09-24 14:41:12 -0400

    add route to create new musician

--- src/app.js
+++ src/app.js
@@ -25,4 +25,13 @@ app.get("/musicians/:id", async (req, res, next) => {
   }
 });
 
+app.post("/musicians", async (req, res, next) => {
+  try {
+    const newMusician = await Musician.create(req.body);
+    res.json(newMusician);
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = app;
